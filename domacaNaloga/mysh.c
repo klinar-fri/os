1f966ab6bff97c2c4bc557eade7b19ab50820162
jk <jaka.klinar1@gmail.com>
2026-04-04 16:51:11 +0200

    debug deluje skoraj pravilno

--- domacaNaloga/mysh.c
+++ domacaNaloga/mysh.c
@@ -5,8 +5,15 @@
 #include <unistd.h>
 #include <ctype.h>
 
-// Koliko je največja velikost enega ukaza -> vprašanje
-#define MAX_LINE_SIZE 2048
+#define MAX_LINE_SIZE 1024 
+int DEBUG_LEVEL = 0;
+
+typedef struct{
+    const char* ime;
+    const char* pomoc;
+    // kazalec na funkcijo
+    int (*fPtr)(int*, char**);
+} Ukaz;
 
 // neka struktura samo za potrebe funkicje
 // zato ker nimamo parov v C
@@ -17,7 +24,6 @@ typedef struct{
     bool hasComment;
     int commentPointer;
 } LineInfo;
-
 
 LineInfo stringCleanup(char* line){
     LineInfo li;
@@ -65,48 +71,117 @@ LineInfo stringCleanup(char* line){
     return li;
 }
 
-void parseTokens(int* tokenCount, char** tokens){
+
+int debug(int* tokenCount, char** tokens){
+    int tc = *tokenCount;
+    if(tc > 1){
+        int j = 0;
+        bool pravaOblika = true;
+        while(tokens[1][j] != '\0'){
+            if(isalpha(tokens[1][j])){
+                DEBUG_LEVEL = 0;
+                pravaOblika = false;
+            }
+            j++;
+        }
+        if(pravaOblika) DEBUG_LEVEL = atoi(tokens[1]);
+    }else{
+        printf("%d\n", DEBUG_LEVEL);
+    }
+    return 0;
+}
+
+int executeBuiltin(Ukaz u, int* tokenCount, char** tokens, bool* ozadje){
+    if(DEBUG_LEVEL > 0){
+        if(*ozadje){
+            printf("Executing builtin '%s' in background\n", tokens[0]);
+        }else{
+            printf("Executing builtin '%s' in foreground\n", tokens[0]);
+        }
+    }
+    u.fPtr(tokenCount, tokens);
+}
+
+int executeExternal(int* tokenCount, char** tokens){
+    printf("External command '");
+    bool first = true;
+    for(int i = 0; i < *tokenCount; i++){
+        if(first){
+            printf("%s", tokens[i]);
+            first = false;
+        }else{
+            printf(" %s", tokens[i]);
+        }
+    } 
+    printf("'\n");
+    return 0;
+}
+
+Ukaz ukazi[] = {
+    {"debug", "pomoc ni implementirana!", &debug},
+    // {"prompt", "pomoc ni implementirana!", prompt}
+};
+
+int findBuiltin(int* tokenCount, char** tokens, bool* ozadje){
+    int steviloUkazov = sizeof(ukazi) / sizeof(Ukaz);
+    for(int i = 0; i < steviloUkazov; i++){
+        if(strcmp(tokens[0], ukazi[i].ime) == 0){
+            return executeBuiltin(ukazi[i], tokenCount, tokens, ozadje);
+        }
+    }
+    return executeExternal(tokenCount, tokens);
+}
+
+void parseTokens(int tokenCount, char** tokens){
     bool preusmeritevIzhoda = false;
     char* pIPtr = NULL;
     bool preusmeritevVhoda = false;
     char* pVPtr = NULL;
     bool ozadje = false;
-    *tokenCount -= 1;
-    int diff = 3 < *tokenCount - 1 ? 3 : *tokenCount - 1;
+    int tknCpy = tokenCount;
+
+    tokenCount -= 1;
+    int diff = 3 < tokenCount - 1 ? 3 : tokenCount - 1;
 
     for(int i = 0; i < diff; i++){
-        if(tokens[*tokenCount][0] == '&' && strlen(tokens[*tokenCount]) == 1){
+        if(tokens[tokenCount][0] == '&' && strlen(tokens[tokenCount]) == 1){
             ozadje = true;
         }
-        if(tokens[*tokenCount][0] == '>'){
+        if(tokens[tokenCount][0] == '>'){
             preusmeritevIzhoda = true;
-            pIPtr = tokens[*tokenCount] + 1;
+            pIPtr = tokens[tokenCount] + 1;
         }
-        if(tokens[*tokenCount][0] == '<'){
+        if(tokens[tokenCount][0] == '<'){
             preusmeritevVhoda= true;
-            pVPtr = tokens[*tokenCount] + 1;
+            pVPtr = tokens[tokenCount] + 1;
         }
-        *tokenCount -= 1;
+        tokenCount -= 1;
     }
-    if(preusmeritevVhoda){
-        printf("Input redirect: '%s'\n", pVPtr);
+
+    if(DEBUG_LEVEL > 0){
+        if(preusmeritevVhoda){
+            printf("Input redirect: '%s'\n", pVPtr);
+        }
+        if(preusmeritevIzhoda){
+            printf("Output redirect: '%s'\n", pIPtr);
+        }
+        if(ozadje){
+            printf("Background: 1\n");
+        }
     }
-    if(preusmeritevIzhoda){
-        printf("Output redirect: '%s'\n", pIPtr);
-    }
-    if(ozadje){
-        printf("Background: 1\n");
-    }
+
+    int status = findBuiltin(&tknCpy, tokens, &ozadje);
+    // printf("STATUS: %d\n", status);
 }
 
-void tokenizeInput(char* imeLupine, char* line, char** tokens, char version){
+int tokenizeInput(char* imeLupine, char* line, char** tokens, char version){
     int tokenCount = 0;
     int lineSize = strlen(line);
     if(line[lineSize - 1] == '\n') line[lineSize - 1] = '\0';
 
     LineInfo li = stringCleanup(line);
-    printf("Input line: '%s'\n", line);
-    if(li.onlySpace) return;
+    if(DEBUG_LEVEL > 0) printf("Input line: '%s'\n", line);
+    if(li.onlySpace) return 0;
 
     int i = 0;
     char* zacetek = li.line;
@@ -139,10 +214,12 @@ void tokenizeInput(char* imeLupine, char* line, char** tokens, char version){
             tokens[i] = ptr += 1;
         }
     }
-    for(int j = 0; j < tokenCount; j++){
-        printf("Token %d: '%s'\n", j, tokens[j]);
+    if(DEBUG_LEVEL > 0){
+        for(int j = 0; j < tokenCount; j++){
+            printf("Token %d: '%s'\n", j, tokens[j]);
+        }
     }
-    parseTokens(&tokenCount, tokens);
+    return tokenCount;
 }
 
 int main(int argc, char** argv){
@@ -156,10 +233,12 @@ int main(int argc, char** argv){
         if(isatty(STDIN_FILENO)){
             printf("%s", imeLupine);
             fgets(line, MAX_LINE_SIZE, stdin);
-            tokenizeInput(imeLupine, line, tokens, 'I');
+            int tokenCount = tokenizeInput(imeLupine, line, tokens, 'I');
+            parseTokens(tokenCount, tokens);
         }else{
             if(fgets(line, MAX_LINE_SIZE, stdin) == NULL) break;
-            tokenizeInput(imeLupine, line, tokens, 'N');
+            int tokenCount = tokenizeInput(imeLupine, line, tokens, 'N');
+            parseTokens(tokenCount, tokens);
         }
         free(line);
     }
